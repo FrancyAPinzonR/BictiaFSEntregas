56ac19920ef6920eee299fb1fc774d1b00125304
Francy A Pinzon Ramos <francykale@gmail.com>
2020-08-27 01:31:13 -0500

    Ajustes finales

--- Calculadora/assets/js/script.js
+++ Calculadora/assets/js/script.js
@@ -1,29 +1,35 @@
-let num1 = parseInt(document.getElementById('num1').value)
-let num2 = parseInt(document.getElementById('num2').value)
+let num1 = 0
+let num2 = 0
+
 console.log(num1, num2)
 
 let resultado = document.getElementById('respuesta')
 
 function operaciones(operador) {
 
+    console.log(operador)
+    num1 = parseInt(document.getElementById('num1').value)
+    num2 = parseInt(document.getElementById('num2').value)
+
     switch (operador) {
         case "suma":
             resultado.innerText = num1 + num2
-            console.log(num1, resultado)
+            console.log(num1, num2, resultado)
             break;
         case "resta":
             resultado.innerText = num1 - num2
-            console.log(resultado)
+            console.log(num1, num2, resultado)
             break;
         case "multiplicacion":
             resultado.innerText = num1 * num2
-            console.log(resultado)
+            console.log(num1, num2, resultado)
             break;
         case "division":
             resultado.innerText = num1 / num2
-            console.log(resultado)
+            console.log(num1, num2, resultado)
             break;
         default:
+            resultado.innerText = "No Valido"
             console.log('Operacion No Valida')
             break;
 
