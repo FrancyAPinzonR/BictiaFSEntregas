79f8f2e44032c05957308b33806683cd44e72b18
Francy A Pinzon Ramos <francykale@gmail.com>
2020-09-21 12:48:03 -0500

    Ejercicio LogicaLS

--- TallerLogicaLS/assest/js/profile.js
+++ TallerLogicaLS/assest/js/profile.js
@@ -1,12 +1,47 @@
-const btnList = document.getElementById('btnList');
+const personEnStorage = JSON.parse(localStorage.getItem('person'))
+console.log(personEnStorage)
+// const inputUserName = document.getElementById('inputUserame').value
+const nameperson = document.getElementById('nameperson')
+console.log(nameperson.innerText)
+
+for (let i = 0; i < personEnStorage.length; i += 1) {
+    nameperson.innerText = personEnStorage[i].nombre
+    console.log(nameperson)
+}
+
+
+
+
+const letras = document.querySelectorAll('.letra')
+// console.log(letras)
 
 const inputList = document.getElementById('inputList');
 
+const teclado = (e) => {
+    const textoTarget = e.target.innerText
+    if (textoTarget == "SPACE") {
+        inputList.innerHTML += "&nbsp"
+    } else if (textoTarget == "DELETE") {
+        inputList.innerHTML = inputList.innerText.slice(0, -1)
+
+    } else {
+        inputList.innerHTML += textoTarget
+    }
+}
+
+for (let letra of letras) {
+    letra.addEventListener('click', teclado)
+}
+
+
+const btnList = document.getElementById('btnList');
+
 function enterList() {
-    const inputList = document.getElementById('inputList').value;
-    const list = document.getElementById('list');
+    // const inputList = document.getElementById('inputList').value;
+    console.log(inputList.innerText)
+    const list = document.getElementById('lista');
     const itemList = document.createElement('li');
-    itemList.textContent = inputList;
+    itemList.textContent = inputList.innerText;
     list.appendChild(itemList);
     itemList.addEventListener('click', () => {
         itemList.classList.toggle('tachado');
@@ -19,4 +54,5 @@ function enterList() {
 
 btnList.onclick = function () {
     enterList();
+
 };
